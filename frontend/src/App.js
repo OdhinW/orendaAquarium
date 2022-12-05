eeef27c96eded595ed56b1b5701d645313cdfe76
OdhinW <rasangaodhin@gmail.com>
2022-12-06 03:23:38 +0530

    start

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Orenda Aquarium</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
